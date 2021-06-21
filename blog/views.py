7843e1b8dc6cb8c6b7a0bcd20d65bcb208f6b5b9
mythxn <mithain3@icloud.com>
2021-06-21 17:14:32 +0400

    (blog): revamp
    - major code refactor (remove common code to base.html)
    - connected bootstrap
    - static vs dynamic pages (templates)
    - passing data to front-end
    - linking front-end pages using page names from urlconf
    - add blog to installed_apps (django settings)

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "myth",
+        "title": "hello_world",
+        "content": "first post content",
+        "date_posted": "Aug 27, 2018",
+    },
+    {
+        "author": "jasonbourne",
+        "title": "post_2",
+        "content": "hmm",
+        "date_posted": "Aug 28, 2021",
+    },
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
